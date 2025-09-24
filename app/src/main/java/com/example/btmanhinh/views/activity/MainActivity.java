320be1e5c44ff2895a8d9f2b5874720e45fed157
huyhoangy <huyhoangwf@gmail.com>
2025-09-24 11:28:38 +0700

    commit sign up

--- app/src/main/java/com/example/btmanhinh/views/activity/MainActivity.java
+++ app/src/main/java/com/example/btmanhinh/views/activity/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.btmanhinh.views.activity;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.btmanhinh.R;
 // Nhớ import đúng đường dẫn tới LoginFragment của bạn
 import com.example.btmanhinh.views.fragments.LoginFragment;
+import com.example.btmanhinh.views.fragments.SignupFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +22,17 @@ public class MainActivity extends AppCompatActivity {
                     .replace(R.id.fragment_container_view, LoginFragment.class, null)
                     .commit();
         }
+    }
+
+
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        transaction.replace(R.id.fragment_container_view, fragment);
+
+
+        transaction.addToBackStack(null);
+
+        transaction.commit();
     }
 }
--- app/src/main/java/com/example/btmanhinh/views/fragments/LoginFragment.java
+++ app/src/main/java/com/example/btmanhinh/views/fragments/LoginFragment.java
@@ -4,15 +4,40 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.btmanhinh.R;
+import com.example.btmanhinh.views.activity.MainActivity;
 
 public class LoginFragment extends Fragment {
+    private TextView txtSignup;
+
+    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_login, container, false);
     }
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        init( view);
+        setupCLickListener();
+    }
+    public void init(View view){
+        txtSignup=view.findViewById(R.id.txt_Signup);
+    }
+    public void setupCLickListener(){
+        txtSignup.setOnClickListener(v -> {
+            if (getActivity() instanceof MainActivity) {
+                ((MainActivity) getActivity()).openFragment(new SignupFragment());
+            }
+        });
+
+
+    }
+
 }
